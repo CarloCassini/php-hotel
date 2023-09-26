2e2cd05c3dd0c0354a841dca8440bcd7f355106e
CarloCassini <c.cassini87@gmail.com>
2023-09-26 16:11:26 +0200

    add table

--- index.php
+++ index.php
@@ -61,10 +61,49 @@ $hotels = [
 
 <body>
 
-    <?php foreach ($hotels as $hotel) {
-        var_dump($hotel);
-        echo $hotel['name'];
-    } ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">name</th>
+                <th scope="col">description</th>
+                <th scope="col">parking</th>
+                <th scope="col">vote</th>
+                <th scope="col">distance_to_center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $key => $hotel) { ?>
+                <tr>
+                    <th scope="row">
+                        <?php echo $key + 1 ?>
+                    </th>
+                    <td>
+                        <?php echo $hotel['name'] ?>
+                    </td>
+                    <td>
+
+                        <?php echo $hotel['description'] ?>
+                    </td>
+                    <td>
+
+                        <?php echo $hotel['parking'] ?>
+                    </td>
+                    <td>
+
+                        <?php echo $hotel['vote'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['distance_to_center'] ?>
+
+                    </td>
+
+                </tr>
+            <?php } ?>
+
+        </tbody>
+    </table>
+
 
 </body>
 
