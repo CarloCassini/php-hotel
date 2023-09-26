d38c6c22049b0c40d02fab3cb0eddf13dceae9a4
CarloCassini <c.cassini87@gmail.com>
2023-09-26 16:14:28 +0200

    terminata 1a milestone

--- index.php
+++ index.php
@@ -61,48 +61,52 @@ $hotels = [
 
 <body>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">name</th>
-                <th scope="col">description</th>
-                <th scope="col">parking</th>
-                <th scope="col">vote</th>
-                <th scope="col">distance_to_center</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($hotels as $key => $hotel) { ?>
+    <div class="container my-5">
+
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">name</th>
+                    <th scope="col">description</th>
+                    <th scope="col">parking</th>
+                    <th scope="col">vote</th>
+                    <th scope="col">distance_to_center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $key => $hotel) { ?>
                 <tr>
                     <th scope="row">
-                        <?php echo $key + 1 ?>
+                        <?php echo $key + 1; ?>
                     </th>
                     <td>
-                        <?php echo $hotel['name'] ?>
+                        <?php echo $hotel['name']; ?>
                     </td>
                     <td>
 
-                        <?php echo $hotel['description'] ?>
+                        <?php echo $hotel['description']; ?>
                     </td>
                     <td>
 
-                        <?php echo $hotel['parking'] ?>
+                        <?php echo $hotel['parking']; ?>
                     </td>
                     <td>
 
-                        <?php echo $hotel['vote'] ?>
+                        <?php echo $hotel['vote']; ?>
                     </td>
                     <td>
-                        <?php echo $hotel['distance_to_center'] ?>
+                        <?php echo $hotel['distance_to_center']; ?>
 
                     </td>
 
                 </tr>
-            <?php } ?>
+                <?php } ?>
 
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
 
 
 </body>
